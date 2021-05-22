49358e0b72eadf64efbfb897469918d5e83c0409
Gabriel L. Pontani <gleonelpontani@gmail.com>
2021-05-22 17:13:32 -0300

    README.md

---
+++ src/testedeapi/Controllers/HealthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testedeapi.Business.Abstractions.Interfaces;
+
+namespace testedeapi.Api.Controllers {
+    [ApiController]
+    [Route("/v1/[controller]")]
+    public class HealthController : ControllerBase {
+        private readonly IPacienteService service;
+
+        public HealthController(IPacienteService service) {
+            this.service = service;
+        }
+
+        /// <summary>
+        /// Testar se a API está funcionando
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("")]
+        
+        public string Health(
+            ) {            
+            return "OK";
+        }        
+    }
+}
