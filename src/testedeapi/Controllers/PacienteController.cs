62b4a7650b6b3c916ea236ba6b33d0f5a8edeef7
Gabriel L. Pontani <gleonelpontani@gmail.com>
2021-05-22 16:49:40 -0300

    API final

--- src/testedeapi/Controllers/PacienteController.cs
+++ src/testedeapi/Controllers/PacienteController.cs
@@ -5,11 +5,6 @@ using testedeapi.Business.Abstractions.DTO;
 using testedeapi.Business.Abstractions.Interfaces;
 
 namespace testedeapi.Api.Controllers {
-
-
-    /// <summary>
-    /// Pacites
-    /// </summary>
     [ApiController]
     [Route("/v1/[controller]")]
     public class PacienteController : ControllerBase {
@@ -23,11 +18,11 @@ namespace testedeapi.Api.Controllers {
         /// <summary>
         /// Lista pacientes
         /// </summary>
-        /// <param name="skip">aa</param>
-        /// <param name="pageSize">aaa</param>
-        /// <param name="count"></param>
-        /// <param name="inativos"></param>
-        /// <param name="pesquisa"></param>
+        /// <param name="skip"> Quantidade de pacientes que deseja pular para paginação</param>
+        /// <param name="pageSize"> Quantidade de paciente que deseja listar</param>
+        /// <param name="count">    Quatidade de pacientes no banco para calcular a quantidade de páginas</param>
+        /// <param name="inativos"> Pacientes inativados</param>
+        /// <param name="pesquisa"> Pesquisa de acordo com o nome</param>
         /// <returns></returns>
         [HttpGet]
         [Route("")]
@@ -41,23 +36,59 @@ namespace testedeapi.Api.Controllers {
             return service.List(skip, pageSize, count, inativos, pesquisa);
         }
 
+        /// <summary>
+        /// Trazer o paciente de acordo com o Id dele
+        /// </summary>
+        /// <param name="Idpaciente">   Id do paciente</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("{Idpaciente}")]
         public PacienteDTO Get(Guid Idpaciente) {
             return service.GetById(Idpaciente);
         }
 
+        /// <summary>
+        /// Inserir paciente
+        /// </summary>
+        /// <param name="paciente">Informações do paciente:
+        /// 
+        /// Validações:
+        ///     Nome: É obrigatório e precisa ter entre 3 e 200 caracteres;
+        ///     Sexo: É obrigatório e precisa ser inserido apenas "M"(Masculino), "F"(Feminino), "O" (Outro), "P" (Prefiro não declarar);
+        ///     Nascimento: É obrigatório e precisa ser uma data entre 1900/01/01 e a data atual;
+        ///     Inativo: Caso queira deixar o paciente inativo inserir a data que foi inativado;
+        ///     Celular: Não é obrigatório e pode ser inserido no mínimo 13 caracteres ((11)1111-1111) e máximo de 14 ((11)91111-1111);
+        /// </param>
         [HttpPost]
         public void Insert(PacienteInsertDTO paciente) {            
             service.Insert(paciente);
         }
 
+        /// <summary>
+        /// Inativar o paciente
+        /// </summary>
+        /// <param name="Idpaciente">Id do paciente a ser inativado</param>
         [HttpDelete]
         [Route("{Idpaciente}")]
         public void Delete(Guid Idpaciente) {
             service.Delete(Idpaciente);
         }
 
+        /// <summary>
+        /// Atualizar um paciente
+        /// </summary>
+        /// <param name="Idpaciente">Id o paciente a ser atualizado
+        ///     *É obrigatório
+        /// </param>
+        /// <param name="paciente">Informações do paciente atualizada:        
+        ///         
+        /// Validações:
+        ///     Nome: É obrigatório e precisa ter entre 3 e 200 caracteres;
+        ///     Sexo: É obrigatório e precisa ser inserido apenas "M"(Masculino), "F"(Feminino), "O" (Outro), "P" (Prefiro não declarar);
+        ///     Nascimento: É obrigatório e precisa ser uma data entre 1900/01/01 e a data atual;
+        ///     Inativo: Caso queira deixar o paciente inativo inserir a data que foi inativado;
+        ///     Celular: Não é obrigatório e pode ser inserido no mínimo 13 caracteres ((11)1111-1111) e máximo de 14 ((11)91111-1111);
+        /// </param>
         [HttpPut]
         [Route("{Idpaciente}")]
         public void Update(Guid Idpaciente, PacienteDTO paciente) {
